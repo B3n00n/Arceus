75f89524f2fc14d9edf1af1add352b34a19fc730
Benoon <72699618+B3n00n@users.noreply.github.com>
2026-01-05 17:09:15 +0200

    local tests

--- src-tauri/src/app/models/config.rs
+++ src-tauri/src/app/models/config.rs
@@ -32,7 +32,8 @@ pub struct AlakazamConfig {
 impl Default for AlakazamConfig {
     fn default() -> Self {
         Self {
-            base_url: "https://alakazam-yexfczgpca-uc.a.run.app".to_string(),
+            //base_url: "https://alakazam-yexfczgpca-uc.a.run.app".to_string(),
+            base_url: "http://localhost:43571".to_string(),
             snorlax_endpoint: "/api/arcade/snorlax/latest".to_string(),
         }
     }
